WebTrit/janus-gateway

commit 1c6d80e9da5573e6b2854cc3d15d225ffff9000c

Allow plugins to specify msid in SDPs (#2998)

src/sdp.c
@@ -291,7 +291,7 @@ int janus_sdp_process_remote(void *ice_handle, janus_sdp *remote_sdp, gboolean r
 			temp = temp->next;
 			continue;
 		}
-		/* Look for mid, ICE credentials and fingerprint first: check media attributes */
+		/* Look for mid, msid, ICE credentials and fingerprint first: check media attributes */
 		GList *tempA = m->attributes;
 		while(tempA) {
 			janus_sdp_attribute *a = (janus_sdp_attribute *)tempA->data;
@@ -320,6 +320,26 @@ int janus_sdp_process_remote(void *ice_handle, janus_sdp *remote_sdp, gboolean r
 					}
 					if(handle->pc_mid == NULL)
 						handle->pc_mid = g_strdup(a->value);
+				} else if(!strcasecmp(a->name, "msid")) {
+					/* Found msid attribute */
+					char msid[65], mstid[65];
+					msid[0] = '\0';
+					mstid[0] = '\0';
+					if(sscanf(a->value, "%64s %64s", msid, mstid) != 2) {
+						JANUS_LOG(LOG_ERR, "[%"SCNu64"] Invalid msid on m-line #%d\n",
+							handle->handle_id, m->index);
+						return -2;
+					}
+					if(medium->remote_msid == NULL || strcasecmp(medium->remote_msid, msid)) {
+						char *old_msid = medium->remote_msid;
+						medium->remote_msid = g_strdup(msid);
+						g_free(old_msid);
+					}
+					if(medium->remote_mstid == NULL || strcasecmp(medium->remote_mstid, mstid)) {
+						char *old_mstid = medium->remote_mstid;
+						medium->remote_mstid = g_strdup(mstid);
+						g_free(old_mstid);
+					}
 				} else if(!strcasecmp(a->name, "fingerprint")) {
 					JANUS_LOG(LOG_VERB, "[%"SCNu64"] Fingerprint (local) : %s\n", handle->handle_id, a->value);
 					if(strcasestr(a->value, "sha-256 ") == a->value) {
@@ -731,8 +751,9 @@ int janus_sdp_process_local(void *ice_handle, janus_sdp *remote_sdp, gboolean up
 			else
 				medium = janus_ice_peerconnection_medium_create(handle, JANUS_MEDIA_UNKNOWN);
 		}
-		/* Check if the offer contributed an mid */
+		/* Check if the offer contributed a mid and/or msid */
 		GList *tempA = m->attributes;
+		gboolean have_msid = FALSE;
 		while(tempA) {
 			janus_sdp_attribute *a = (janus_sdp_attribute *)tempA->data;
 			if(a->name && a->value) {
@@ -757,6 +778,33 @@ int janus_sdp_process_local(void *ice_handle, janus_sdp *remote_sdp, gboolean up
 					tempA = tempA->next;
 					m->attributes = g_list_remove_link(m->attributes, mid_attr);
 					g_list_free_full(mid_attr, (GDestroyNotify)janus_sdp_attribute_destroy);
+					continue;
+				} else if(!strcasecmp(a->name, "msid")) {
+					/* Found msid attribute */
+					have_msid = TRUE;
+					char msid[65], mstid[65];
+					msid[0] = '\0';
+					mstid[0] = '\0';
+					if(sscanf(a->value, "%64s %64s", msid, mstid) != 2) {
+						JANUS_LOG(LOG_ERR, "[%"SCNu64"] Invalid msid on m-line #%d\n",
+							handle->handle_id, m->index);
+						return -2;
+					}
+					if(medium->msid == NULL || strcasecmp(medium->msid, msid)) {
+						char *old_msid = medium->msid;
+						medium->msid = g_strdup(msid);
+						g_free(old_msid);
+					}
+					if(medium->mstid == NULL || strcasecmp(medium->mstid, mstid)) {
+						char *old_mstid = medium->mstid;
+						medium->mstid = g_strdup(mstid);
+						g_free(old_mstid);
+					}
+					/* Remove this msid attribute, the core will add it again later */
+					GList *msid_attr = tempA;
+					tempA = tempA->next;
+					m->attributes = g_list_remove_link(m->attributes, msid_attr);
+					g_list_free_full(msid_attr, (GDestroyNotify)janus_sdp_attribute_destroy);
 					continue;
 				} else if(!strcasecmp(a->name, "rtpmap")) {
 					if(a->value && strstr(a->value, "rtx") == NULL) {
@@ -788,6 +836,12 @@ int janus_sdp_process_local(void *ice_handle, janus_sdp *remote_sdp, gboolean up
 				g_hash_table_insert(pc->media_bymid, g_strdup(medium->mid), medium);
 				janus_refcount_increase(&medium->ref);
 			}
+		}
+		if(!have_msid) {
+			g_free(medium->msid);
+			medium->msid = NULL;
+			g_free(medium->mstid);
+			medium->mstid = NULL;
 		}
 		if(m->direction == JANUS_SDP_INACTIVE) {
 			/* FIXME Reset the local SSRCs and RTCP context */
@@ -1297,7 +1351,6 @@ int janus_sdp_anonymize(janus_sdp *anon) {
 					|| !strcasecmp(a->name, "setup")
 					|| !strcasecmp(a->name, "connection")
 					|| !strcasecmp(a->name, "group")
-					|| !strcasecmp(a->name, "msid")
 					|| !strcasecmp(a->name, "msid-semantic")
 					|| !strcasecmp(a->name, "rid")
 					|| !strcasecmp(a->name, "simulcast")
@@ -1433,7 +1486,7 @@ char *janus_sdp_merge(void *ice_handle, janus_sdp *anon, gboolean offer) {
 	a = janus_sdp_attribute_create("extmap-allow-mixed", NULL);
 	anon->attributes = g_list_insert_before(anon->attributes, first, a);
 	/* msid-semantic: add new global attribute */
-	a = janus_sdp_attribute_create("msid-semantic", " WMS janus");
+	a = janus_sdp_attribute_create("msid-semantic", " WMS *");
 	anon->attributes = g_list_insert_before(anon->attributes, first, a);
 	/* ICE Full or Lite? */
 	if(janus_ice_is_ice_lite_enabled()) {
@@ -1594,8 +1647,14 @@ char *janus_sdp_merge(void *ice_handle, janus_sdp *anon, gboolean offer) {
 			m->attributes = g_list_append(m->attributes, a);
 		}
 		if(m->type == JANUS_SDP_AUDIO || m->type == JANUS_SDP_VIDEO) {
-			a = janus_sdp_attribute_create("msid", "janus janus%s", medium->mid);
-			m->attributes = g_list_append(m->attributes, a);
+			if(m->direction != JANUS_SDP_INACTIVE) {
+				if(medium->msid && medium->mstid) {
+					a = janus_sdp_attribute_create("msid", "%s %s", medium->msid, medium->mstid);
+				} else {
+					a = janus_sdp_attribute_create("msid", "janus janus%s", medium->mid);
+				}
+				m->attributes = g_list_append(m->attributes, a);
+			}
 			if(medium->ssrc > 0) {
 				a = janus_sdp_attribute_create("ssrc", "%"SCNu32" cname:janus", medium->ssrc);
 				m->attributes = g_list_append(m->attributes, a);

conf/janus.plugin.streaming.jcfg.sample.in
@@ -155,7 +155,8 @@ rtp-sample: {
 # each identifying a single stream to add, that will then translate to
 # a dedicated m-line in the SDP. For each stream, you specify the type,
 # a unique ID (mid), and can provide a short description (label) so that
-# the client side can know what's what when rendering the streams. Notice
+# the client side can know what's what when rendering the streams;
+# optionally, a msid to add to the SDP m-line can be provided as well. Notice
 # how the port/pt/codec/fmtp/etc. stuff is called just like that, without
 # any audio/video/data prefix: in fact, each media stream can be configured
 # the same way, and it's the type that allows us to differentiate them.

html/mvideoroomtest.js
@@ -25,6 +25,7 @@ var doSimulcast = (getQueryStringValue("simulcast") === "yes" || getQueryStringV
 var acodec = (getQueryStringValue("acodec") !== "" ? getQueryStringValue("acodec") : null);
 var vcodec = (getQueryStringValue("vcodec") !== "" ? getQueryStringValue("vcodec") : null);
 var subscriber_mode = (getQueryStringValue("subscriber-mode") === "yes" || getQueryStringValue("subscriber-mode") === "true");
+var use_msid = (getQueryStringValue("msid") === "yes" || getQueryStringValue("msid") === "true");
 
 $(document).ready(function() {
 	// Initialize the library (all console debuggers enabled)
@@ -630,6 +631,7 @@ function subscribeTo(sources) {
 					room: myroom,
 					ptype: "subscriber",
 					streams: subscription,
+					use_msid: use_msid,
 					private_id: mypvtid
 				};
 				remoteFeed.send({ message: subscribe });

html/videoroomtest.js
@@ -25,6 +25,7 @@ var acodec = (getQueryStringValue("acodec") !== "" ? getQueryStringValue("acodec
 var vcodec = (getQueryStringValue("vcodec") !== "" ? getQueryStringValue("vcodec") : null);
 var doDtx = (getQueryStringValue("dtx") === "yes" || getQueryStringValue("dtx") === "true");
 var subscriber_mode = (getQueryStringValue("subscriber-mode") === "yes" || getQueryStringValue("subscriber-mode") === "true");
+var use_msid = (getQueryStringValue("msid") === "yes" || getQueryStringValue("msid") === "true");
 
 $(document).ready(function() {
 	// Initialize the library (all console debuggers enabled)
@@ -550,6 +551,7 @@ function newRemoteFeed(id, display, streams) {
 					room: myroom,
 					ptype: "subscriber",
 					streams: subscription,
+					use_msid: use_msid,
 					private_id: mypvtid
 				};
 				remoteFeed.send({ message: subscribe });

src/ice.c
@@ -1829,6 +1829,10 @@ static void janus_ice_peerconnection_medium_dereference(janus_ice_peerconnection
 static void janus_ice_peerconnection_medium_free(const janus_refcount *medium_ref) {
 	janus_ice_peerconnection_medium *medium = janus_refcount_containerof(medium_ref, janus_ice_peerconnection_medium, ref);
 	g_free(medium->mid);
+	g_free(medium->msid);
+	g_free(medium->mstid);
+	g_free(medium->remote_msid);
+	g_free(medium->remote_mstid);
 	g_free(medium->rid[0]);
 	medium->rid[0] = NULL;
 	g_free(medium->rid[1]);

src/ice.h
@@ -512,6 +512,8 @@ struct janus_ice_peerconnection_medium {
 	int mindex;
 	/*! \brief Media ID */
 	char *mid;
+	/*! \brief Media Stream ID info */
+	char *msid, *mstid, *remote_msid, *remote_mstid;
 	/*! \brief SSRC of the server for this medium */
 	guint32 ssrc;
 	/*! \brief Retransmission SSRC of the server for this medium */

src/janus.c
@@ -3213,6 +3213,20 @@ json_t *janus_admin_peerconnection_medium_summary(janus_ice_peerconnection_mediu
 		json_object_set_new(m, "type", json_string("data"));
 	json_object_set_new(m, "mindex", json_integer(medium->mindex));
 	json_object_set_new(m, "mid", json_string(medium->mid));
+	if(medium->msid || medium->remote_msid) {
+		json_t *mm = json_object();
+		if(medium->msid) {
+			json_object_set_new(mm, "local-stream", json_string(medium->msid));
+			if(medium->mstid)
+				json_object_set_new(mm, "local-track", json_string(medium->mstid));
+		}
+		if(medium->remote_msid) {
+			json_object_set_new(mm, "remote-stream", json_string(medium->remote_msid));
+			if(medium->remote_mstid)
+				json_object_set_new(mm, "remote-track", json_string(medium->remote_mstid));
+		}
+		json_object_set_new(m, "msid", mm);
+	}
 	if(medium->type != JANUS_MEDIA_DATA) {
 		json_object_set_new(m, "do_nacks", medium->do_nacks ? json_true() : json_false());
 		json_object_set_new(m, "nack-queue-ms", json_integer(medium->nack_queue_ms));
@@ -3799,12 +3813,40 @@ json_t *janus_plugin_handle_sdp(janus_plugin_session *plugin_session, janus_plug
 			janus_ice_peerconnection_medium *medium = ice_handle->pc ?
 				g_hash_table_lookup(ice_handle->pc->media, GUINT_TO_POINTER(mindex)) : NULL;
 			gboolean have_mid = FALSE, have_rid = FALSE, have_repaired_rid = FALSE,
-				have_twcc = FALSE, have_dd = FALSE, have_abs_send_time = FALSE;
+				have_twcc = FALSE, have_dd = FALSE, have_abs_send_time = FALSE, have_msid = FALSE;
 			int opusred_pt = -1;
 			GList *tempA = m->attributes;
 			while(tempA) {
 				janus_sdp_attribute *a = (janus_sdp_attribute *)tempA->data;
-				if(a->name && a->value && !strcasecmp(a->name, "extmap")) {
+				if(a->name && a->value && !strcasecmp(a->name, "msid")) {
+					/* Found msid attribute */
+					have_msid = TRUE;
+					char msid[65], mstid[65];
+					msid[0] = '\0';
+					mstid[0] = '\0';
+					if(sscanf(a->value, "%64s %64s", msid, mstid) != 2) {
+						JANUS_LOG(LOG_ERR, "[%"SCNu64"] Invalid msid on m-line #%d\n",
+							ice_handle->handle_id, m->index);
+						janus_sdp_destroy(parsed_sdp);
+						return NULL;
+					}
+					if(medium->msid == NULL || strcasecmp(medium->msid, msid)) {
+						char *old_msid = medium->msid;
+						medium->msid = g_strdup(msid);
+						g_free(old_msid);
+					}
+					if(medium->mstid == NULL || strcasecmp(medium->mstid, mstid)) {
+						char *old_mstid = medium->mstid;
+						medium->mstid = g_strdup(mstid);
+						g_free(old_mstid);
+					}
+					/* Remove this msid attribute, the core will add it again later */
+					GList *msid_attr = tempA;
+					tempA = tempA->next;
+					m->attributes = g_list_remove_link(m->attributes, msid_attr);
+					g_list_free_full(msid_attr, (GDestroyNotify)janus_sdp_attribute_destroy);
+					continue;
+				} else if(a->name && a->value && !strcasecmp(a->name, "extmap")) {
 					if(strstr(a->value, JANUS_RTP_EXTMAP_MID))
 						have_mid = TRUE;
 					else if(strstr(a->value, JANUS_RTP_EXTMAP_RID))
@@ -3826,6 +3868,12 @@ json_t *janus_plugin_handle_sdp(janus_plugin_session *plugin_session, janus_plug
 			/* If the user offered RED but the plugin rejected it, disable it */
 			if(opusred_pt < 0 && medium != NULL && medium->opusred_pt > 0)
 				medium->opusred_pt = 0;
+			if(!have_msid) {
+				g_free(medium->msid);
+				medium->msid = NULL;
+				g_free(medium->mstid);
+				medium->mstid = NULL;
+			}
 			/* Check if rid-based simulcasting is available */
 			if(!have_rid && medium != NULL) {
 				g_free(medium->rid[0]);

commit f662bda32e1ee364a71f108e34a061a04385bd9f

fix(sip): forward video re-INVITE to browser when no local video port (WT-1541)
When a SIP re-INVITE adds video to an audio-only call and Janus has no
local video port allocated yet, bypass autoaccept_reinvites and forward
the offer to the browser. This allows the browser to generate a real SDP
answer (e.g. recvonly when no camera is available) instead of Janus
responding immediately with a default sendrecv SDP.

Also fix janus_sdp_merge() to not synthesize msid/ssrc lines for
recvonly m-lines without a real plugin-provided msid. Synthesizing
SSRCs on a recvonly section misleads Chrome into expecting RTP from
those SSRCs, which corrupts the audio decoder state on the peer side.
The synthesis bug was introduced upstream in commit 1c6d80e9 (2022).

src/plugins/janus_sip.c
@@ -6528,18 +6528,25 @@ void janus_sip_sofia_callback(nua_event_t event, int status, char const *phrase,
 				}
 			}
 			if(reinvite && session->media.autoaccept_reinvites) {
-				if(session->media.earlymedia_video_recovery) {
-					/* Case B: re-INVITE is adding video after audio-only early media (PortaSwitch
-					 * pattern).  Do NOT auto-accept blindly — the browser must renegotiate WebRTC to
-					 * activate its video transceiver and produce a proper SDP answer.  Clear the flag
-					 * and fall through to the normal re-INVITE handling path below, which will send
-					 * an updatingcall to the browser; the browser's update answer will call
-					 * nua_respond(200, sdp) with the correct SDP body. */
-					JANUS_LOG(LOG_VERB, "earlymedia_video_recovery (Case B): re-INVITE adds video "
-						"(port=%d), bypassing auto-accept, forwarding to browser\n",
+				/* Detect a re-INVITE that adds video to an audio-only call: janus_sip_sdp_process()
+				 * above already updated has_video/local_video_rtp_port from the new offer, so
+				 * has_video=TRUE with local_video_rtp_port==0 means Janus has no video port yet. */
+				gboolean video_added_to_audio_call =
+					session->media.has_video && session->media.local_video_rtp_port == 0;
+				if(session->media.earlymedia_video_recovery || video_added_to_audio_call) {
+					/* Case B / video-add: re-INVITE is adding video — do NOT auto-accept blindly.
+					 * The browser must renegotiate WebRTC to activate its video transceiver and
+					 * produce a proper SDP answer (e.g. recvonly when no camera is available).
+					 * Clear the flag and fall through to the normal re-INVITE handling path below,
+					 * which will send an updatingcall to the browser; the browser's update answer
+					 * will call nua_respond(200, sdp) with the correct SDP body. */
+					JANUS_LOG(LOG_VERB, "re-INVITE adds video (earlymedia_video_recovery=%d, "
+						"video_added_to_audio_call=%d, port=%d): bypassing auto-accept, "
+						"forwarding to browser\n",
+						session->media.earlymedia_video_recovery, video_added_to_audio_call,
 						session->media.remote_video_rtp_port);
 					session->media.earlymedia_video_recovery = FALSE;
-					/* fall through */
+					/* fall through — forward to browser to get real SDP answer */
 				} else {
 					/* No need to involve the application: we reply ourselves */
 					nua_respond(nh, 200, sip_status_phrase(200), TAG_END());

src/sdp.c
@@ -1688,15 +1688,25 @@ char *janus_sdp_merge(void *ice_handle, janus_sdp *anon, gboolean offer) {
 			m->attributes = g_list_append(m->attributes, a);
 		}
 		if(m->type == JANUS_SDP_AUDIO || m->type == JANUS_SDP_VIDEO) {
+			gboolean has_real_msid = (medium->msid != NULL && medium->mstid != NULL);
+			gboolean janus_is_sending = (m->direction != JANUS_SDP_INACTIVE &&
+			                              m->direction != JANUS_SDP_RECVONLY);
 			if(m->direction != JANUS_SDP_INACTIVE) {
-				if(medium->msid && medium->mstid) {
+				if(has_real_msid) {
+					/* Plugin provided a real msid: always include it (for recvonly too,
+					 * as the remote end needs to identify the track). */
 					a = janus_sdp_attribute_create("msid", "%s %s", medium->msid, medium->mstid);
-				} else {
+					m->attributes = g_list_append(m->attributes, a);
+				} else if(janus_is_sending) {
+					/* Janus is sending media but has no plugin-provided msid: synthesize one. */
 					a = janus_sdp_attribute_create("msid", "janus janus%s", medium->mid);
+					m->attributes = g_list_append(m->attributes, a);
 				}
-				m->attributes = g_list_append(m->attributes, a);
+				/* recvonly without a real msid: do not synthesize — adding a fake msid/ssrc
+				 * on a recvonly section misleads Chrome into expecting RTP from those SSRCs,
+				 * which corrupts the audio decoder state on the peer side. */
 			}
-			if(medium->ssrc > 0) {
+			if(medium->ssrc > 0 && (has_real_msid || janus_is_sending)) {
 				a = janus_sdp_attribute_create("ssrc", "%"SCNu32" cname:janus", medium->ssrc);
 				m->attributes = g_list_append(m->attributes, a);
 				if(medium->ssrc_rtx > 0 && m->type == JANUS_SDP_VIDEO &&